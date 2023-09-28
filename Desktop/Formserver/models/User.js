e8004e761c153582e71b8c85673d968f68952c80
unknown <you@example.com>
2023-09-28 20:47:01 +0530

    first commit

---
+++ Desktop/Formserver/models/User.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    // phone: {
+    //     type: Number,
+    // },
+    // pickLink: {
+    //     type: String,
+    // },
+}, { timestamps: true })
+
+const User = model("User", UserSchema);
+
+export default User;
